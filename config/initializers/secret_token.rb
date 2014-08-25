7aa1940262ed1fb1219359d732dab2bc20e0de2f
Gary C. Davison <gary.davison@jhu.edu>
2014-08-25 17:06:09 -0400

    Tasks4 initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tasks4::Application.config.secret_key_base = '081bbe2d910b2eef1bc38f5473a8fd0d202ba5286b99d759bc37c9374543b86855106938475b6d481e2b9eb725038dedefce868a93a241c1110f1b5f0d7925d4'
